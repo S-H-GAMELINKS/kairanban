fd32290593a73c690f575078a0f0f00af5a8be13
S-H-GAMELINKS <gamelinks007@gmail.com>
2020-12-06 18:16:19 +0900

    migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_06_074609) do
+ActiveRecord::Schema.define(version: 2020_12_06_090948) do
 
   create_table "notices", force: :cascade do |t|
     t.string "title"
@@ -19,6 +19,8 @@ ActiveRecord::Schema.define(version: 2020_12_06_074609) do
     t.text "content"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "to"
+    t.string "from"
   end
 
 end
