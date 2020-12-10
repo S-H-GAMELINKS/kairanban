2ef8a485378303071c9fc689c078b9e4af512ece
S-H-GAMELINKS <gamelinks007@gmail.com>
2020-12-10 13:39:34 +0900

    using kaminari for pagenation

--- app/controllers/notices_controller.rb
+++ app/controllers/notices_controller.rb
@@ -6,7 +6,7 @@ class NoticesController < ApplicationController
   # GET /notices
   # GET /notices.json
   def index
-    @notices = Notice.all
+    @notices = Notice.page params[:page]
   end
 
   # GET /notices/1
--- app/models/notice.rb
+++ app/models/notice.rb
@@ -1,3 +1,4 @@
 class Notice < ApplicationRecord
     belongs_to :user
+    paginates_per 9
 end
