9e39a5e64c8dcd23906351ebae69b43113ac1ec9
S-H-GAMELINKS <gamelinks007@gmail.com>
2021-01-05 23:35:22 +0900

    db rollback

--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_03_131026) do
-
-  create_table "cases", force: :cascade do |t|
-    t.string "title"
-    t.text "content"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-  end
+ActiveRecord::Schema.define(version: 2020_12_13_080848) do
 
   create_table "notices", force: :cascade do |t|
     t.string "title"
