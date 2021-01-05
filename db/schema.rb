0d66a4103533e404dd6069d83bdd8fd575ebc6ab
S-H-GAMELINKS <gamelinks007@gmail.com>
2021-01-05 23:37:14 +0900

    db migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_13_080848) do
+ActiveRecord::Schema.define(version: 2021_01_05_143644) do
+
+  create_table "examples", force: :cascade do |t|
+    t.string "title"
+    t.text "content"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "notices", force: :cascade do |t|
     t.string "title"
