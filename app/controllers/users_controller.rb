a853b9eebc56d9262f32475f7fc019f4258f8b0f
S-H-GAMELINKS <gamelinks007@gmail.com>
2021-01-12 22:29:25 +0900

    Revert "add set_user"
    This reverts commit 65b9d111acb95d86872e7931fe540d590cf88bbd.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,17 +1,7 @@
 class UsersController < ApplicationController
-  before_action :set_user
   def mypage
   end
 
   def edit
   end
-
-  private
-    def set_user
-      if user_signed_in?
-        @user = current_user
-      else
-        redirect_to new_user_session_path
-      end
-    end
 end
