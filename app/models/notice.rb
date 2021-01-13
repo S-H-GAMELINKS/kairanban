f62628153135cdd60c19574a623fa546ceee5236
S-H-GAMELINKS <gamelinks007@gmail.com>
2021-01-13 21:58:18 +0900

    add validates for notice model

--- app/models/notice.rb
+++ app/models/notice.rb
@@ -1,4 +1,10 @@
 class Notice < ApplicationRecord
     belongs_to :user
     paginates_per 9
+    validates :title, presence: true, length: {maximum: 50}
+    validates :to, presence: true, length: {maximum: 30}
+    validates :from, presence: true, length: {maximum: 30}
+    validates :auther, length: {maximum: 20}
+    validates :greet, length: {maximum: 50}
+    validates :content, presence: true, length: {maximum:500}
 end
